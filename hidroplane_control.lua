9a26530ab0227d458fd8c0a845b37c29c5321063
Alexsandro Percy <alexsandro.percy@gmail.com>
2021-11-28 19:46:47 -0300

    sound adjust

--- hidroplane_control.lua
+++ hidroplane_control.lua
@@ -214,7 +214,7 @@ function hidroplane.engineSoundPlay(self)
     if self.sound_handle then minetest.sound_stop(self.sound_handle) end
     self.sound_handle = minetest.sound_play({name = "hidroplane_engine"},
         {object = self.object, gain = 2.0,
-            pitch = 0.5 + ((self._power_lever/100)/2),max_hear_distance = 10,
+            pitch = 0.5 + ((self._power_lever/100)/2),max_hear_distance = 15,
             loop = true,})
 end
 

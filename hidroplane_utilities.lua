de69c657a359a65877e6c248b58ef338bca07f5d
Alexsandro Percy <alexsandro.percy@gmail.com>
2022-01-22 18:10:09 -0300

    fix on dettach passenger

--- hidroplane_utilities.lua
+++ hidroplane_utilities.lua
@@ -154,11 +154,13 @@ function hidroplane.dettach_pax(self, player)
     self._passenger = nil
 
     -- detach the player
-    player:set_detach()
-    player_api.player_attached[name] = nil
-    player_api.set_animation(player, "stand")
-    player:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
+    if player then
+        player:set_detach()
+        player_api.player_attached[name] = nil
+        player_api.set_animation(player, "stand")
+        player:set_eye_offset({x=0,y=0,z=0},{x=0,y=0,z=0})
     --remove_physics_override(player, {speed=1,gravity=1,jump=1})
+    end
 end
 
 function hidroplane.checkAttach(self, player)

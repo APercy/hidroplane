4bb1e25fcd4040e08097ff41e83c95684da541f7
Alexsandro Percy <alexsandro.percy@gmail.com>
2022-05-05 13:52:42 -0300

    another try to fix the sit bug

--- hidroplane_utilities.lua
+++ hidroplane_utilities.lua
@@ -53,7 +53,7 @@ function hidroplane.attach(self, player, instructor_mode)
     player:set_eye_offset({x = 0, y = eye_y, z = 2}, {x = 0, y = 1, z = -30})
     player_api.player_attached[name] = true
     -- make the driver sit
-    minetest.after(0.2, function()
+    minetest.after(1, function()
         player = minetest.get_player_by_name(name)
         if player then
 	        player_api.set_animation(player, "sit")
@@ -606,9 +606,9 @@ function hidroplane.flightstep(self)
     -- end accell
     ------------------------------------------------------
 
-    if player then 
+    --[[if player then 
         hidroplane.attach(self, player, self._instruction_mode)
-    end
+    end]]--
 
     ------------------------------------------------------
     -- sound and animation

67b6a45ec8002fef0b6f0d2b5d7c652f9fbf3c0b
Alexsandro Percy <alexsandro.percy@gmail.com>
2023-07-16 20:57:22 -0300

    removed old entity

--- init.lua
+++ init.lua
@@ -107,7 +107,14 @@ local function fetch_setting(name)
 end
 
 
-local old_entities = {"hidroplane:seat_base","hidroplane:engine","hidroplane:pointer","hidroplane:stick","hidroplane:front_wheels"}
+hidroplane:engine
+hidroplane:front_wheels
+hidroplane:wheels
+hidroplane:stick
+hidroplane:pointer
+hidroplane:seat_base
+
+local old_entities = {"hidroplane:seat_base","hidroplane:engine","hidroplane:pointer","hidroplane:stick","hidroplane:front_wheels","hidroplane:wheels"}
 for _,entity_name in ipairs(old_entities) do
     minetest.register_entity(":"..entity_name, {
         on_activate = function(self, staticdata)
